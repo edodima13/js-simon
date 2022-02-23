432a22b13772c384c0aad8a18b768f5c49c42548
edodima13 <edodima13@libero.it>
2022-02-23 15:59:32 +0100

    avanzamento

--- myjavascript.js
+++ myjavascript.js
@@ -1,9 +1,20 @@
-//visualizzare cinque numeri casuali
+//visualizzare cinque numeri casuali ``
 //creare un array vuoto
-  //fare una funione che generi numeri casuali da 1 a 10
-  //dichiarare il range dei numeri casuali
+let numeriCasuali = [];
+  //fare una funzione che generi numeri casuali
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+//dichiarare il range dei numeri casuali
+const min = 1;
+const max = 10;
+  
 //inserire i numeri all'interno di un array vuoto
-  //inserire i numeri dell'array all'interno di un alert
+
+//inserire i numeri dell'array all'interno di un alert
+
 //generare un timer per inserire i numeri 
  //alla fine del timer deve inserire i numeri con dei prompt uno alla volta
 //comunicare quali e quanti numeri sono giusti
@@ -13,11 +24,7 @@
 
 
 
-/*function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+/*
 
 let numberAlert1 = getRandomInt( 1, 10 )
 console.log( numberAlert )
