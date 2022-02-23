fd837be40a4d240f3449d5c506ed2562f528b912
edodima13 <edodima13@libero.it>
2022-02-23 15:40:55 +0100

    divisione dell'esercizio

---
+++ myjavascript.js
@@ -0,0 +1,25 @@
+//visualizzare cinque numeri casuali
+//creare un array vuoto
+  //fare una funione che generi numeri casuali da 1 a 10
+  //dichiarare il range dei numeri casuali
+//inserire i numeri all'interno di un array vuoto
+  //inserire i numeri dell'array all'interno di un alert
+//generare un timer per inserire i numeri 
+ //alla fine del timer deve inserire i numeri con dei prompt uno alla volta
+//comunicare quali e quanti numeri sono giusti
+
+
+
+
+
+
+/*function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let numberAlert1 = getRandomInt( 1, 10 )
+console.log( numberAlert )
+
+const messaggio = alert(numberAlert)*/
